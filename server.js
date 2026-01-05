d50409416a66c57bbaac178433a8d90ac5bebe18
chirazboudh <37347461+chirazboudh@users.noreply.github.com>
2026-01-05 12:25:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -20,19 +20,19 @@ client.on('close', () => {
 });
 const TOKEN_SECRET = 'SECRET123';
 
-app.post('/open', (req, res) => {
+app.get('/open', (req, res) => {
   if(req.body.token !== TOKEN_SECRET) return res.status(403).send('Token invalide');
   client.publish('aquatec/porte/cmd', 'YB'); // ouvrir
   res.send('Porte ouverte');
 });
 
-app.post('/close', (req, res) => {
+app.get('/close', (req, res) => {
   if(req.body.token !== TOKEN_SECRET) return res.status(403).send('Token invalide');
   client.publish('aquatec/porte/cmd', 'YP'); // fermer
   res.send('Porte fermée');
 });
 
-app.post('/stop', (req, res) => {
+app.get('/stop', (req, res) => {
   if(req.body.token !== TOKEN_SECRET) return res.status(403).send('Token invalide');
   client.publish('aquatec/porte/cmd', 'ST'); // stop
   res.send('Commande STOP envoyée');
@@ -40,3 +40,4 @@ app.post('/stop', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Serveur prêt sur le port ${PORT}`));
+
